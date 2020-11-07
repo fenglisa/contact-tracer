fa03639bb1ac5f64a7241e105f050c46824adb67
fenglisa <lisafeng10@gmail.com>
2020-11-07 13:52:34 -0500

    added uuid

--- frontend/src/reducers/checkInsReducer.js
+++ frontend/src/reducers/checkInsReducer.js
@@ -1,7 +1,13 @@
+import { v4 as uuid } from 'uuid';
+
 function checkInsReducer(state = [], action) {
   switch (action.type) {
     case "ADD_CHECKIN":
-      return [...state, action.checkIn];
+      const checkIn = {
+        id: uuid(),
+        name: action.checkIn
+      }
+      return state.concat(checkIn);
 
     case "DELETE_CHECKIN":
       return state.filter(c => c.id !== action.id);
--- frontend/src/reducers/symptomsReducer.js
+++ frontend/src/reducers/symptomsReducer.js
@@ -1,7 +1,13 @@
+import { v4 as uuid } from 'uuid';
+
 function symptomsReducer(state = [], action) {
   switch (action.type) {
     case "ADD_SYMPTOM":
-      return [...state, action.symptom];
+    const symptom = {
+      id: uuid(),
+      name: action.symptom
+    }
+    return state.concat(symptom);
 
     case "DELETE_SYMPTOM":
       return state.filter(s => s.id !== action.id);
