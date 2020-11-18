0360399da87718aa27ac197f20a59f97360fcad9
fenglisa <lisafeng10@gmail.com>
2020-11-18 12:12:40 -0500

    add only unique symptoms to store

--- frontend/src/reducers/symptomsReducer.js
+++ frontend/src/reducers/symptomsReducer.js
@@ -3,11 +3,16 @@ import { v4 as uuid } from 'uuid';
 function symptomsReducer(state = [], action) {
   switch (action.type) {
     case "ADD_SYMPTOM":
-    const symptom = {
-      id: uuid(),
-      name: action.symptom
-    }
-    return state.concat(symptom);
+      if(state.filter(s => s.name === action.symptom).length > 0){
+        alert('You have already logged that symptom');
+        return state;
+      }else{
+        const symptom = {
+          id: uuid(),
+          name: action.symptom
+        };
+        return state.concat(symptom);
+      };
 
     case "DELETE_SYMPTOM":
       return state.filter(s => s.id !== action.id);
