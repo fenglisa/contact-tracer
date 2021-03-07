6a683201f26af579f73df827fbb8e646b2c9e075
fenglisa <lisafeng10@gmail.com>
2021-03-06 22:22:23 -0500

    disable like functionality

--- frontend/src/components/checkins/CheckIn.js
+++ frontend/src/components/checkins/CheckIn.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+// import Likes from './Likes';
 
 class CheckIn extends Component {
+
   handleOnClick = () => {
     this.props.deleteCheckIn(this.props.checkIn.id)
   }
@@ -20,3 +22,37 @@ class CheckIn extends Component {
 };
 
 export default CheckIn;
+
+// state = {
+//   clicks: 0
+// }
+
+// handleClick = () => {
+//   this.setState({
+//     clicks: this.state.clicks + 1
+//   })
+// }
+
+// callAPI = () => {
+//   console.log('a')
+//   fetch('http://localhost:3000/fdsfdsfds').then(r => {
+//     if(r.ok) {
+//       console.log(r, 'b')
+//       return r.json()
+//     } else {
+//       throw new Error('Error')
+//     }
+//   })
+//   .then(data => {
+//     console.log('c', data)
+//   })
+//   .catch(err => console.log('d', err))
+//   console.log('e')
+//
+//   // a e b c
+//
+//   // a e d
+// }
+
+// <button onClick={this.callAPI}> Upvote </button>
+// < Likes upvotes={this.state.clicks}/>
