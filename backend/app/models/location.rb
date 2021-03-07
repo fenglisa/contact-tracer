0c7b627dab5dc7e447194a155cf321645fb54953
fenglisa <lisafeng10@gmail.com>
2021-03-06 22:25:00 -0500

    normalize name validation for checkins

--- backend/app/models/location.rb
+++ backend/app/models/location.rb
@@ -1,5 +1,11 @@
 class Location < ApplicationRecord
   validates :name, presence: true
+  before_validation :normalize_name
   has_many :check_ins
   has_many :users, through: :check_ins
+
+  private
+  def normalize_name
+    self.name = name.downcase.titleize
+  end
 end
